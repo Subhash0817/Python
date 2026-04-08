d1a46830bebe44736c9198b6cc23aadb0d9a1a43
Subhash <subhashbantu88@gmail.com>
2026-04-08 15:16:05 +0530

    updated code - added new function

--- practice.py
+++ practice.py
@@ -1,3 +1,6 @@
+from numpy import add, number
+
+
 name = 'subh'
 age = 21
 city = 'hyderabad'
@@ -75,3 +78,44 @@ else:
 
 
 
+for i in range(1, 11):
+    print(i)
+
+for  i in range(1, 20):
+    if i%2 ==0:
+        print(f'{i} is an even number')
+
+for i in range(1, 20):
+    if i%2 != 0:
+        print(f'{i} is an odd number')  
+
+for i in range(1, 11):
+    print(f'{5} x {i} = {5*i}')
+
+    
+
+
+def add(a,b):
+    return a + b
+result = add(10, 20)
+print(f'the sum of 10 and 20 is {result}')
+
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+number = 17
+if is_even(number):
+    print(f'{number} is an even number')        
+else:    print(f'{number} is an odd number')
+
+
+
+
+def table(n):
+    for i in range(1, 11):
+        print(f'{n} x {i} = {n*i}')
+number = 5
+table(number)   
