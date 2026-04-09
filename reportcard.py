0dbefe68b25d9dcaa5bb90090f8d658bf21e2527
Subhash <subhashbantu88@gmail.com>
2026-04-09 14:54:20 +0530

    added python file

--- reportcard.py
+++ reportcard.py
@@ -7,3 +7,17 @@ if calculate_average(student['marks']) >= 50:
     print('pass')
 else:
     print('fail')
+
+
+
+student = input("enter the name of the student: ")
+
+subjects = ['maths', 'physics', 'chemistry', 'english', 'computer science']
+marks = []
+for i in subjects:
+    marks .append(int(input(f'enter the marks for {i}: ')))
+average = sum(marks) / len(marks)
+if average <= 40:
+    print('fail')
+elif average >= 40:
+    print('pass')
